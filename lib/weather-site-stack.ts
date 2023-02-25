5a3ecea3e189ab6e9e39f180f2257736ec954ee8
Danielle Heberling <danielle.heberling@navabenefits.com>
2023-02-25 15:50:54 -0800

    Init commit

--- lib/weather-site-stack.ts
+++ lib/weather-site-stack.ts
@@ -1,10 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class WeatherSiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // The code that defines your stack goes here
 
