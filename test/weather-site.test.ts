1504b06d5bc000053af2e45c3211f2ed0d629f08
Danielle Heberling <danielle.heberling@navabenefits.com>
2023-02-25 15:42:24 -0800

    Initial commit

---
+++ test/weather-site.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WeatherSite from '../lib/weather-site-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/weather-site-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WeatherSite.WeatherSiteStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
