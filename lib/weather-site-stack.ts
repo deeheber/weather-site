48cb3870e0603024b3c337c3620af8621bc0a63a
Danielle Heberling <danielle.heberling@navabenefits.com>
2023-02-26 11:16:16 -0800

    Base infra

--- lib/weather-site-stack.ts
+++ lib/weather-site-stack.ts
@@ -1,36 +1,81 @@
-import * as cdk from 'aws-cdk-lib'
+import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Bucket } from 'aws-cdk-lib/aws-s3'
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda'
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs'
+import {
+  LogLevel,
+  Pass,
+  StateMachine,
+  StateMachineType,
+} from 'aws-cdk-lib/aws-stepfunctions'
 
-export class WeatherSiteStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class WeatherSiteStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, 'WeatherSiteBucket', {
+      // TODO: add and parameterize bucket name
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+      // TODO: Revisit this to possibly RETAIN
+      removalPolicy: RemovalPolicy.DESTROY,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'WeatherSiteQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const table = new Table(this, 'WeatherSiteTable', {
+      partitionKey: { name: 'id', type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY,
+    })
 
-    /**
-     * TODO:
-     * - S3 bucket
-     *  - Parameterized bucket name
-     *  - Enable static website hosting
-     * - DynamoDB table
-     *  - Partition key: PK
-     *  - Capacity mode: on-demand ???
-     * - Lambda function
-     *  - Parameterized Weather API key
-     *  - Parameterized Weather Location Lat
-     *  - Parameterized Weather Location Lon
-     *  - Parameterized Weather Type
-     *  - Write code for Lambda function
-     * - Step Function State Machine
-     *  - SF execution role (S3 PutObject, DDB GetItem UpdateItem, Lambda Invoke)
-     *  - SF definition
-     * - EventBridge Scheduler to invoke SF every 15 minutes
-     */
+    const checkCurrentWeatherFunction = new NodejsFunction(
+      this,
+      'WeatherSiteFunction',
+      {
+        functionName: 'WeatherSiteFunction',
+        runtime: Runtime.NODEJS_18_X,
+        entry: 'dist/src/functions/weather-site-function.js',
+        logRetention: RetentionDays.ONE_WEEK,
+        architecture: Architecture.ARM_64,
+        timeout: Duration.seconds(30),
+        memorySize: 3008,
+        /* TODO: Add env vars as params
+         *  Weather API key
+         *  Weather Location Lat
+         *  Weather Location Lon
+         *  Weather Type
+         * (https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2)
+         */
+      }
+    )
+
+    const logGroup = new LogGroup(this, 'WeatherSiteLogGroup', {
+      logGroupName: 'WeatherSiteLogGroup',
+      retention: RetentionDays.ONE_WEEK,
+      removalPolicy: RemovalPolicy.DESTROY,
+    })
+
+    const stateMachine = new StateMachine(this, 'WeatherSiteStateMachine', {
+      stateMachineName: 'WeatherSiteStateMachine',
+      stateMachineType: StateMachineType.EXPRESS,
+      tracingEnabled: true,
+      logs: {
+        destination: logGroup,
+        includeExecutionData: true,
+        // TODO: Revisit this to possibly set to ERROR
+        level: LogLevel.ALL,
+      },
+      // TODO: Write SF definition
+      definition: new Pass(this, 'StartState'),
+    })
+
+    // IAM Permissions
+    bucket.grantWrite(stateMachine)
+    table.grantReadWriteData(stateMachine)
+    checkCurrentWeatherFunction.grantInvoke(stateMachine)
+
+    // TODO: Add EventBridge Scheduler to invoke SF every 15 minutes
   }
 }
