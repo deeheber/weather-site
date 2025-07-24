b7d42f5c8e4aa019bc8606e6f33982b90d4ded8e
Danielle Heberling <deeheber@users.noreply.github.com>
2025-07-24 09:58:55 -0700

    Update tests

--- test/weather-site.test.ts
+++ test/weather-site.test.ts
@@ -1,12 +1,12 @@
 import { App } from 'aws-cdk-lib'
 import { Template } from 'aws-cdk-lib/assertions'
 
+import { DomainStack } from '../lib/domain-stack'
 import { WeatherSiteStack } from '../lib/weather-site-stack'
 
-test('Verify resources are created', () => {
+test('Verify resources are created for non-custom domain site', () => {
   const app = new App()
   const stack = new WeatherSiteStack(app, 'MyTestStack', {
-    domainName: '',
     locationName: 'Test Location',
     openWeatherUrl: 'https://api.openweathermap.org/data/2.5/onecall',
     schedules: 'rate(10 minutes)'.split(', '),
@@ -26,7 +26,6 @@ test('Verify resources are created', () => {
     Runtime: 'nodejs22.x',
     Architectures: ['arm64'],
   })
-
   template.hasResourceProperties('AWS::StepFunctions::StateMachine', {
     StateMachineName: 'MyTestStack-state-machine',
     StateMachineType: 'EXPRESS',
@@ -34,7 +33,6 @@ test('Verify resources are created', () => {
       Level: 'ALL',
     },
   })
-
   template.hasResourceProperties('AWS::Scheduler::Schedule', {
     Name: 'MyTestStack-schedule-0',
     ScheduleExpression: 'rate(10 minutes)',
@@ -50,70 +48,105 @@ test('Verify resources are created', () => {
   expect(template.toJSON()).toMatchSnapshot()
 })
 
-test('Verify resources are created with custom domain', () => {
-  const app = new App()
-  const stack = new WeatherSiteStack(app, 'CustomDomainStack', {
-    domainName: 'mydomain.com',
-    locationName: 'New York, NY USA',
-    openWeatherUrl: 'https://api.openweathermap.org/data/2.5/onecall',
-    schedules:
-      'cron(0/30 * * 6-9 ? *), cron(0/10 * * 1,2,3,4,5,10,11,12 ? *)'.split(
-        ', ',
-      ),
-    weatherLocationLat: '111',
-    weatherLocationLon: '222',
-    weatherType: 'rain',
-  })
-  const template = Template.fromStack(stack)
+describe('Custom domain resources', () => {
+  test('Verify domain stack resources', () => {
+    const app = new App()
+    const domainName = 'mydomain.com'
 
-  template.resourceCountIs('AWS::S3::Bucket', 1)
-  template.resourceCountIs('AWS::SSM::Parameter', 1)
-  template.resourceCountIs('AWS::CloudFront::Distribution', 2)
-  template.resourceCountIs('AWS::Route53::RecordSet', 2)
-  template.resourceCountIs('AWS::CloudFront::Function', 1)
-  template.resourceCountIs('AWS::Events::Connection', 1)
+    const domainStack = new DomainStack(app, 'TestDomainStack', {
+      domainName,
+    })
+    const template = Template.fromStack(domainStack)
 
-  template.hasResourceProperties('AWS::CertificateManager::Certificate', {
-    DomainName: 'mydomain.com',
-  })
-  template.hasResourceProperties('AWS::CertificateManager::Certificate', {
-    DomainName: 'www.mydomain.com',
-  })
-  template.hasResourceProperties('AWS::Lambda::Function', {
-    FunctionName: 'CustomDomainStack-updateSiteFunction',
-    Runtime: 'nodejs22.x',
-    Architectures: ['arm64'],
+    template.resourceCountIs('AWS::Route53::HostedZone', 1)
+    template.resourceCountIs('AWS::CertificateManager::Certificate', 2)
+    template.resourceCountIs('AWS::CloudFront::Function', 1)
+    template.resourceCountIs('AWS::CloudFront::Distribution', 1)
+    template.resourceCountIs('AWS::Route53::RecordSet', 1)
+
+    template.hasResourceProperties('AWS::Route53::HostedZone', {
+      Name: 'mydomain.com.',
+    })
+    template.hasResourceProperties('AWS::CertificateManager::Certificate', {
+      DomainName: 'mydomain.com',
+    })
+    template.hasResourceProperties('AWS::CertificateManager::Certificate', {
+      DomainName: 'www.mydomain.com',
+    })
+    template.hasResourceProperties('AWS::Route53::RecordSet', {
+      Name: 'www.mydomain.com.',
+      Type: 'A',
+    })
   })
 
-  template.hasResourceProperties('AWS::StepFunctions::StateMachine', {
-    StateMachineName: 'CustomDomainStack-state-machine',
-    StateMachineType: 'EXPRESS',
-    LoggingConfiguration: {
-      Level: 'ALL',
-    },
-  })
-  template.hasResourceProperties('AWS::Scheduler::Schedule', {
-    Name: 'CustomDomainStack-schedule-0',
-    ScheduleExpression: 'cron(0/30 * * 6-9 ? *)',
-    Target: {
-      Input: JSON.stringify({
-        WEATHER_TYPE: 'rain',
-        WEATHER_LOCATION_LAT: '111',
-        WEATHER_LOCATION_LON: '222',
-      }),
-    },
-  })
-  template.hasResourceProperties('AWS::Scheduler::Schedule', {
-    Name: 'CustomDomainStack-schedule-1',
-    ScheduleExpression: 'cron(0/10 * * 1,2,3,4,5,10,11,12 ? *)',
-    Target: {
-      Input: JSON.stringify({
-        WEATHER_TYPE: 'rain',
-        WEATHER_LOCATION_LAT: '111',
-        WEATHER_LOCATION_LON: '222',
-      }),
-    },
-  })
+  test('Verify weather stack with custom domain', () => {
+    const app = new App()
+    const domainName = 'mydomain.com'
 
-  expect(template.toJSON()).toMatchSnapshot()
+    const domainStack = new DomainStack(app, 'TestDomainStack', {
+      domainName,
+    })
+
+    const weatherStack = new WeatherSiteStack(app, 'TestWeatherStack', {
+      domainName,
+      hostedZone: domainStack?.hostedZone,
+      locationName: 'New York, NY USA',
+      openWeatherUrl: 'https://api.openweathermap.org/data/2.5/onecall',
+      schedules:
+        'cron(0/30 * * 6-9 ? *), cron(0/10 * * 1,2,3,4,5,10,11,12 ? *)'.split(
+          ', ',
+        ),
+      weatherLocationLat: '111',
+      weatherLocationLon: '222',
+      weatherType: 'rain',
+    })
+    const template = Template.fromStack(weatherStack)
+
+    template.resourceCountIs('AWS::S3::Bucket', 1)
+    template.resourceCountIs('AWS::SSM::Parameter', 1)
+    template.resourceCountIs('AWS::CloudFront::Distribution', 1)
+    template.resourceCountIs('AWS::Route53::RecordSet', 1)
+    template.resourceCountIs('AWS::Events::Connection', 1)
+
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      FunctionName: 'TestWeatherStack-updateSiteFunction',
+      Runtime: 'nodejs22.x',
+      Architectures: ['arm64'],
+    })
+    template.hasResourceProperties('AWS::StepFunctions::StateMachine', {
+      StateMachineName: 'TestWeatherStack-state-machine',
+      StateMachineType: 'EXPRESS',
+      LoggingConfiguration: {
+        Level: 'ALL',
+      },
+    })
+    template.hasResourceProperties('AWS::Scheduler::Schedule', {
+      Name: 'TestWeatherStack-schedule-0',
+      ScheduleExpression: 'cron(0/30 * * 6-9 ? *)',
+      Target: {
+        Input: JSON.stringify({
+          WEATHER_TYPE: 'rain',
+          WEATHER_LOCATION_LAT: '111',
+          WEATHER_LOCATION_LON: '222',
+        }),
+      },
+    })
+    template.hasResourceProperties('AWS::Scheduler::Schedule', {
+      Name: 'TestWeatherStack-schedule-1',
+      ScheduleExpression: 'cron(0/10 * * 1,2,3,4,5,10,11,12 ? *)',
+      Target: {
+        Input: JSON.stringify({
+          WEATHER_TYPE: 'rain',
+          WEATHER_LOCATION_LAT: '111',
+          WEATHER_LOCATION_LON: '222',
+        }),
+      },
+    })
+    template.hasResourceProperties('AWS::Route53::RecordSet', {
+      Name: 'mydomain.com.',
+      Type: 'A',
+    })
+
+    expect(template.toJSON()).toMatchSnapshot()
+  })
 })
