1eae74b977a064a15e466bd7c2a81ef5ad7bb5ce
Danielle Heberling <deeheber@users.noreply.github.com>
2025-11-22 12:02:25 -0800

    Remove aws-cdk-lib.aws_lambda.FunctionOptions#logRetention warnings

--- lib/weather-site-stack.ts
+++ lib/weather-site-stack.ts
@@ -154,11 +154,17 @@ export class WeatherSiteStack extends Stack {
     }
 
     // Upload CSS file to bucket
+    const uploadAssetsLogId = `${this.id}-uploadAssetsLogGroup`
+    const uploadAssetsLog = new LogGroup(this, uploadAssetsLogId, {
+      logGroupName: `/aws/lambda/${uploadAssetsLogId}`,
+      retention: RetentionDays.ONE_WEEK,
+      removalPolicy: RemovalPolicy.DESTROY,
+    })
     new BucketDeployment(this, `${this.id}-file-upload`, {
       sources: [Source.asset(path.join(__dirname, '../src/site'))],
       destinationBucket: this.bucket,
       prune: false,
-      logRetention: RetentionDays.ONE_WEEK,
+      logGroup: uploadAssetsLog,
     })
 
     new CfnOutput(this, `${this.id}-url`, {
